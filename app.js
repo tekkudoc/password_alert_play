29c7de93a73d1296abaa495cfaedda7991afd025
Ray Auxey <rayauxey@gmail.com>
2019-07-31 14:33:12 +0300

    Hashed correct password

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ signin.addEventListener('click', function(e) {
     let username = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
 
+    usernameHashed = CryptoJS.SHA256(username).toString()
+    passwordHashed = CryptoJS.SHA256(password).toString()
+
+    const correctUsernameHashed = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+    const correctPasswordHashed = '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4';
+
     // Alert sounds
     let access_grant = document.querySelector('#access_grant');
     let access_deny = document.querySelector('#access_deny');
@@ -22,7 +28,8 @@ signin.addEventListener('click', function(e) {
     // Card holding Form inputs
     let card = document.querySelector('.card');
     
-    if (username !== 'test' || password !== '1234'){
+    if (usernameHashed != correctUsernameHashed 
+        || passwordHashed != correctPasswordHashed){
         // Access denied alert sound start
         access_deny.play();
         
